0afc3cd28a56eaa4190742e03ec1f472dd7cb897
AliKazempour <Alikazempoor83@gmail.com>
2023-05-21 18:22:36 +0330

    Class Student

--- p2.py
+++ p2.py
@@ -1,9 +1,24 @@
 def factorial(n):
-    if(n==0 or n==1):
+    if (n == 0 or n == 1):
         return 1
     else:
         return n*factorial(n-1)
-print("Hello Ali")        
 
-for i in range(1,5):
+
+print("Hello Ali")
+
+for i in range(1, 5):
     print("Your Friendly Neighborhood Spiderman:)")
+
+
+class Student:
+    def __init__(self, Id, name, family):
+        self.Id = Id
+        self.name = name
+        self.family = family
+
+    def ShowInfo(self):
+        print(f"Id:{self.Id},name:{self.name},family:{self.family}")
+
+s1=Student(100,"Ali","Kazempour")
+s1.ShowInfo()        
